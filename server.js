1cd0c68a0ce0d1b8418de3323ca3a52771f88597
n8m <name.xru@gmail.com>
2014-03-04 22:04:04 +0300

    mailgrid

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
-var nodemailer = require("nodemailer");
+var sendgrid  = require('sendgrid')(
+  process.env.SENDGRID_USERNAME,
+  process.env.SENDGRID_PASSWORD
+);
 
 var app = express();
 app.use(express.static(path.join(__dirname, "static"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
@@ -9,34 +12,14 @@ app.use(express.json());       // to support JSON-encoded bodies
 
 app.post('/mail', function(request, response){
 
-	// create reusable transport method (opens pool of SMTP connections)
-	var smtpTransport = nodemailer.createTransport("SMTP",{
-	    service: "Gmail",
-	    auth: {
-	        user: "blackvilt@gmail.com",
-	        pass: "0291511391"
-	    }
-	});
-
-	// setup e-mail data with unicode symbols
-	var mailOptions = {
-	    from: "blackvilt@gmail.com", // sender address
-	    to: "blackvilt@gmail.com", // list of receivers
-	    subject: "Письмо с сайта", // Subject line
-	    text: request.body.name + '\n' + request.body.mail + '\n' + request.body.msg, // plaintext body
-	    html: "<b>Hello world ✔</b>" // html body
-	}
-
-	// send mail with defined transport object
-	smtpTransport.sendMail(mailOptions, function(error, response){
-	    if(error){
-	        console.log(error);
-	    }else{
-	        console.log("Message sent: " + response.message);
-	    }
-
-	    // if you don't want to use this transport object anymore, uncomment following line
-	    smtpTransport.close(); // shut down the connection pool, no more messages
+	sendgrid.send({
+	  to: 'blackvilt@gmail.com',
+	  from: 'app22711431@heroku.com',
+	  subject: 'Hello World',
+	  text: request.body.name + '\n' + request.body.mail + '\n' + request.body.msg 
+	}, function(err, json) {
+	if (err) { return console.error(err); }
+	  console.log(json);
 	});
 
 });
