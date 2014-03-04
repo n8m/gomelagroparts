8da346238b5aef575d0bf718216e995993200d20
n8m <name.xru@gmail.com>
2014-03-04 20:23:25 +0300

    mail

--- server.js
+++ server.js
@@ -10,8 +10,36 @@ app.use(express.urlencoded()); // to support URL-encoded bodies
 app.post('/mail', function(request, response){
 
 	response.send(request.body.name);
-    // console.log();
-    // console.log(request.body.user.email);
+	mailmsg
+	// create reusable transport method (opens pool of SMTP connections)
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+	    service: "Gmail",
+	    auth: {
+	        user: "blackvilt@gmail.com",
+	        pass: "0291511391"
+	    }
+	});
+
+	// setup e-mail data with unicode symbols
+	var mailOptions = {
+	    from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
+	    to: "blackvilt@gmail.com", // list of receivers
+	    subject: "Письмо с сайт", // Subject line
+	    text: request.body.name + '\n' + request.body.mail + '\n' + request.body.msg, // plaintext body
+	    html: "<b>Hello world ✔</b>" // html body
+	}
+
+	// send mail with defined transport object
+	smtpTransport.sendMail(mailOptions, function(error, response){
+	    if(error){
+	        console.log(error);
+	    }else{
+	        console.log("Message sent: " + response.message);
+	    }
+
+	    // if you don't want to use this transport object anymore, uncomment following line
+	    //smtpTransport.close(); // shut down the connection pool, no more messages
+	});
 
 });
 
